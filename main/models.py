86a80b3d47a5c072a2ea80f71be2d581fae61b41
doston <dost7ony6@gmail.com>
2026-02-28 13:11:48 -0500

    templates

--- main/models.py
+++ main/models.py
@@ -11,4 +11,15 @@ created_at = models.DateTimeField(auto_now_add=True)
 
 def _str_(self):
     return self.title 
-class
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='profile/')
+    location = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
+    github_link = models.URLField(max_length=50)
+    linkedin = models.URLField(max_length=50)
+    def __str__(self):
+        return self.name
