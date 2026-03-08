aa2f90476b65d04c31443188839588eb42eb74d5
akrom_17a <149670082+akrom17ua@users.noreply.github.com>
2026-03-08 21:05:57 +0500

    Update views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Profile
 
-# Create your views here.
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/projects.html', {'projects': projects})
+
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, "portfolio/project_detail.html", {'project': project})
+
+
+def profile(request):
+    profile = Profile.objects.first()
+    return render(request, 'portfolio/profile.html', {'profile': profile})
