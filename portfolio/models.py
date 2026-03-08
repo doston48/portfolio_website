8ffd0d83b0fc7e7f8b2d909bc4dcbf0f603aba65
akrom_17a <149670082+akrom17ua@users.noreply.github.com>
2026-03-08 21:05:35 +0500

    Update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    github_link = models.URLField(blank=True, null=True)
+    technologies = models.TextField(blank=True, null=True)
+    live_link = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='profile/')
+    location = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
+    github_link = models.URLField(max_length=50)
+    linkedin = models.URLField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
